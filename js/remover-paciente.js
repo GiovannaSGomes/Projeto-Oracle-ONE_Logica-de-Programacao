f950acf6df07b1b5eddd7c624559ae6356bf6911
Giovanna Gomes <giovannasg12@gmail.com>
2020-06-21 12:59:05 -0300

    First commit

---
+++ js/remover-paciente.js
@@ -0,0 +1,17 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        if (event.target.tagName == 'TD') {
+            event.target.parentNode.remove();
+        }
+       
+
+    },500);
+
+
+});
